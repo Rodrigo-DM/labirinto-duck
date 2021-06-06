f9b2e3ce027f5886a35f9a6b37b51474dd85e114
Rodrigo-DM <rodrigo.dmnr@gmail.com>
2021-06-06 20:30:45 -0300

    Reorganizando codigo, estilizaçao de botoes e modais

--- assets/script.js
+++ assets/script.js
@@ -16,11 +16,6 @@ const map = [
     "WWWWWWWWWWWWWWWWWWWWW",
 ];
 
-const construirLabrinto = () => {
-    const container = document.getElementById('map');
-    map.forEach((value) => montarDivs(value, container));
-}
-
 const montarDivs = (value, container) => {
     const linha = document.createElement('div');
     linha.classList = ('linha');
@@ -28,20 +23,27 @@ const montarDivs = (value, container) => {
     for (let i = 0; i < value.length; i++) {
         if (value[i] === 'W') {
             const celula = document.createElement('div');
+            
             celula.classList = ('parede');
             linha.appendChild(celula);
+
         } else {
             const space = document.createElement('div');
+
             space.classList = ('vazio');
+
             if (value[i] === 'F') {
                 space.id = 'chegada';
             }
+
             linha.appendChild(space);
 
             if (value[i] === 'S') {
                 const personagem = document.createElement('img');
+
                 personagem.src = './assets/img/personagem.png';
                 personagem.id = 'player';
+
                 space.appendChild(personagem);
             }
         }
@@ -50,33 +52,53 @@ const montarDivs = (value, container) => {
     container.appendChild(linha);
 }
 
-let personLinha = 450;
-let personColuna = 0;
+const construirLabrinto = () => {
+    const container = document.getElementById('map');
+    map.forEach((value) => montarDivs(value, container));
+}
 
-document.addEventListener('keydown', (event) => {
-    const keyName = event.key;
-    const personagem = document.getElementById('player');
-    const caminho = mapear();
+const btnModal = document.getElementById('btn-modal');
 
-    if (keyName === 'ArrowUp') {
+btnModal.addEventListener('click', () => {
+    const modal = document.getElementById('modal-inicial');
+    modal.classList.add('hidden');
+})
 
-        moveLinha(caminho, 1, personagem);
+const chamarModal = (time, position) => {
+    const modal = document.getElementsByClassName('modal')[position];
 
-    } else if (keyName === 'ArrowRight') {
+    setTimeout(() => {
+        modal.classList.remove('hidden');
 
-        moveColuna(caminho, 2, personagem);
+    }, time)
+}
 
-    } else if (keyName === 'ArrowDown') {
+const btnIniciar = document.getElementById('iniciar');
 
-        moveLinha(caminho, 2, personagem);
+btnIniciar.addEventListener('click', () => {
+    const container = document.getElementById('map');
+    container.innerHTML = '';
+    btnIniciar.classList.add('hidden');
 
-    } else if (keyName === 'ArrowLeft') {
+    personLinha = 450;
+    personColuna = 0;
 
-        moveColuna(caminho, 1, personagem);
+    construirLabrinto();
+    const portaLeft = document.getElementById('left');
+    const portaRight = document.getElementById('right');
 
-    }
+    portaLeft.classList.add('anima');
+    portaRight.classList.add('anima');
 
-});
+    setTimeout(() => {
+        portaLeft.classList.remove('anima');
+        portaRight.classList.remove('anima');
+        portaLeft.classList.add('hidden');
+        portaRight.classList.add('hidden');
+    }, 1450);
+
+    chamarModal(1750, 0);
+})
 
 const mapear = () => {
     const space = document.querySelectorAll('.vazio');
@@ -95,52 +117,6 @@ const mapear = () => {
     return caminho;
 }
 
-const moveLinha = (array, signal, personagem) => {
-    if (signal === 1) {
-        personLinha -= 50;
-    } else if (signal === 2) {
-        personLinha += 50;
-    }
-
-    let validaCoord = findCoordenadas(array, personLinha, personColuna);
-
-    if (validaCoord) {
-        personagem.style.top = personLinha + 'px';
-        if (personLinha === 400 && personColuna === 1000) {
-            chamarModal(0, 1)
-        }
-    } else {
-        if (signal === 1) {
-            personLinha += 50;
-        } else if (signal === 2) {
-            personLinha -= 50;
-        }
-    }
-}
-
-const moveColuna = (array, signal, personagem) => {
-    if (signal === 1) {
-        personColuna -= 50;
-    } else if (signal === 2) {
-        personColuna += 50;
-    }
-
-    let validaCoord = findCoordenadas(array, personLinha, personColuna);
-
-    if (validaCoord) {
-        personagem.style.left = personColuna + 'px';
-        if (personLinha === 400 && personColuna === 1000) {
-            chamarModal(0, 1)
-        }
-    } else {
-        if (signal === 1) {
-            personColuna += 50;
-        } else if (signal === 2) {
-            personColuna -= 50;
-        }
-    }
-}
-
 const findCoordenadas = (array, linha, coluna) => {
     let output = false;
 
@@ -153,43 +129,97 @@ const findCoordenadas = (array, linha, coluna) => {
     return output;
 }
 
-const btnIniciar = document.getElementById('iniciar');
+const moveLinha = (array, signal, personagem) => {
+    if (signal === 1) {
+        personLinha -= 50;
 
-btnIniciar.addEventListener('click', () => {
-    btnIniciar.classList.add('hidden')
-    construirLabrinto();
-    const portaLeft = document.getElementById('left');
-    const portaRight = document.getElementById('right');
+    } else if (signal === 2) {
+        personLinha += 50;
+    }
 
-    portaLeft.classList.add('anima');
-    portaRight.classList.add('anima');
+    let validaCoord = findCoordenadas(array, personLinha, personColuna);
 
-    setTimeout(() => {
-        portaLeft.classList.add('hidden');
-        portaRight.classList.add('hidden');
-    }, 1450);
+    if (validaCoord) {
+        personagem.style.top = personLinha + 'px';
 
-    chamarModal(1950, 0);
-})
+        if (personLinha === 400 && personColuna === 1000) {
+            chamarModal(0, 1)
+        }
+    } else {
+        if (signal === 1) {
+            personLinha += 50;
 
-const btnModal = document.getElementById('btn-modal');
-
-btnModal.addEventListener('click', () => {
-    const modal = document.getElementById('modal-inicial');
-    modal.classList.add('hidden');
-})
-
-const chamarModal = (time, position) => {
-    const modal = document.getElementsByClassName('modal')[position];
-
-    setTimeout(() => {
-        modal.classList.remove('hidden');
-    }, time)
+        } else if (signal === 2) {
+            personLinha -= 50;
+        }
+    }
 }
 
-const btnFinal = document.getElementById('final-n');
+const moveColuna = (array, signal, personagem) => {
+    if (signal === 1) {
+        personColuna -= 50;
 
-btnFinal.addEventListener('click', () => {
+    } else if (signal === 2) {
+        personColuna += 50;
+    }
+
+    let validaCoord = findCoordenadas(array, personLinha, personColuna);
+
+    if (validaCoord) {
+        personagem.style.left = personColuna + 'px';
+
+        if (personLinha === 400 && personColuna === 1000) {
+            chamarModal(0, 1)
+        }
+    } else {
+        if (signal === 1) {
+            personColuna += 50;
+
+        } else if (signal === 2) {
+            personColuna -= 50;
+        }
+    }
+}
+
+let personLinha = 450;
+let personColuna = 0;
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    const personagem = document.getElementById('player');
+    const caminho = mapear();
+
+    if (keyName === 'ArrowUp') {
+        moveLinha(caminho, 1, personagem);
+
+    } else if (keyName === 'ArrowRight') {
+        moveColuna(caminho, 2, personagem);
+
+    } else if (keyName === 'ArrowDown') {
+        moveLinha(caminho, 2, personagem);
+
+    } else if (keyName === 'ArrowLeft') {
+        moveColuna(caminho, 1, personagem);
+    }
+});
+
+const btnSim = document.getElementById('final-y');
+
+btnSim.addEventListener('click', () => {
+    const modal = document.getElementsByClassName('modal')[1];
+    modal.classList.add('hidden');
+    const personagem = document.getElementById('player');
+
+    personLinha = 450;
+    personColuna = 0;
+
+    personagem.style.top = personLinha + 'px';
+    personagem.style.left = personColuna + 'px';
+})
+
+const btnNao = document.getElementById('final-n');
+
+btnNao.addEventListener('click', () => {
     const modal = document.getElementsByClassName('modal')[1];
     modal.classList.add('hidden');
 
